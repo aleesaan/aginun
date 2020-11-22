3e01437e17a8043566b19e90e4147070640ca9bd
Alessandro <alesan.t90@gmail.com>
2020-11-22 23:24:15 +0100

    cleanup prettierrc

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,11 +1,4 @@
 module.exports = {
   printWidth: 120,
-  tabWidth: 2,
-  tabs: false,
-  semi: true,
-  quoteProps: "as-needed",
-  tailingComma: "all",
-  bracketSpacing: true,
-  jsxBracketSameLine: false,
   arrowParens: "always"
 };
